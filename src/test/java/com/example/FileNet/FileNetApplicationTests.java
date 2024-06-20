d7a93822806d3684eddefc04be3d3736fe86ea40
Dhanush S <dhanushdh01@gmail.com>
2024-06-21 04:42:06 +0530

    Upload & Download image Added

---
+++ src/test/java/com/example/FileNet/FileNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FileNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
